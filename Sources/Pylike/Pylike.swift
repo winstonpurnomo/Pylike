a0593049bb26b861790ecb2e0c3986427c7d209b
Winston Purnomo <winstonpurnomo@gmail.com>
2022-06-21 22:09:28 +0700

    Added Counter to main

--- Sources/Pylike/Pylike.swift
+++ Sources/Pylike/Pylike.swift
@@ -1,6 +1,32 @@
 public struct Pylike {
     public private(set) var text = "Hello, World!"
 
-    public init() {
+
+    public struct Counter<T: Hashable>: CustomStringConvertible {
+        public var description: String {
+            return dict.description
+        }
+        
+        var dict: [T: Int] = [:]
+        
+        init(_ val: String) where T == String {
+            for char in val {
+                self.dict[String(char), default: 0] += 1
+            }
+        }
+        
+        init(_ val: Array<T>) {
+            for elem in val {
+                self.dict[elem, default: 0] += 1
+            }
+        }
+        
+        func elements() -> [T] {
+            return Array(dict.keys)
+        }
+        
+        mutating func clear() {
+            dict = [:]
+        }
     }
 }
