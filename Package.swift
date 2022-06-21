d558ca1cf9bc2ee1dcfb426424f295bcd960b287
Winston Purnomo <winstonpurnomo@gmail.com>
2022-06-21 22:11:53 +0700

    Changed Package.swift

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Pylike",
-            targets: ["Pylike"]),
+            name: "Counter",
+            targets: ["Counter"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
